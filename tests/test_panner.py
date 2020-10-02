8380592f0dd4bc76d99f9ec80ca5c2d63a1c2ca1
Gabriel Rodrigues <gabrielsr@gmail.com>
2020-10-02 11:26:51 +0200

    removing old test

--- tests/test_panner.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-@pytest.mark.usefixtures("clean_room_mission_manager")
-def test_clean_room_mission_manager(clean_room_mission_manager):
-    plan = clean_room_mission_manager.plan()
-    assert plan is None
